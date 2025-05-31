59309a52ea317a3f1fb207e07b01216ed424cd17
Cyclic3 <cyclic3.git@gmail.com>
2025-05-31 17:23:40 +0100

    Added an example

---
+++ examples/foo.php
@@ -0,0 +1,23 @@
+<?php
+    require_once("../vendor/autoload.php");
+    $remote = new TPEx\TPEx\Remote("https://tpex-staging.cyclic3.dev", trim(file_get_contents("token.txt")));
+    $remote->apply("Deposit", [
+        "player"=>"cyclic3", 
+        "asset"=>"diamond", 
+        "count"=>1, 
+        "banker"=>"#tpex"
+    ]);
+    $remote->apply("BuyCoins", [
+        "player"=>"cyclic3", 
+        "n_diamonds"=>1, 
+    ]);
+    $remote->apply("BuyOrder", [
+        "player"=>"cyclic3",
+        "asset"=>"cobblestone",
+        "count"=>10,
+        "coins_per"=>"10c"
+    ]);
+    $state = $remote->fastsync();
+    print($state->player_balance("cyclic3") . "\n");
+    print(json_encode($state->raw));
+?>
--- src/FastSync.php
+++ src/FastSync.php
@@ -6,6 +6,10 @@
             return $this->raw["balances"]["balances"][$player] ?? "0c";
         }
 
+        function player_assets(string $player) : array {
+            return $this->raw["balances"]["assets"][$player] ?? [];
+        }
+
         function __construct(array $raw) {
             $this->raw = $raw;
         }
